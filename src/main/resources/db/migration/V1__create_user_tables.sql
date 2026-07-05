8e7d1d646c37de81d69748b9d9710073fbba66eb
AncientSun <70993316+Isle4525@users.noreply.github.com>
2026-07-05 19:32:30 +0500

    feat: create users table and roles enum

---
+++ src/main/resources/db/migration/V1__create_user_tables.sql
@@ -0,0 +1,11 @@
+CREATE TYPE user_role AS ENUM ('USER', 'ADMIN');
+
+
+CREATE TABLE users(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role user_role NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
