c2827e52496b9b219561e937f1b05c5140248fea
AncientSun <70993316+Isle4525@users.noreply.github.com>
2026-07-05 20:17:46 +0500

    feat: create worspace model

--- src/main/java/com/example/coworking_booking_system/entity/Workspace.java
+++ src/main/java/com/example/coworking_booking_system/entity/Workspace.java
@@ -1,4 +1,40 @@
 package com.example.coworking_booking_system.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@Entity
+@Table(name = "workspaces")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Workspace {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(nullable = false)
+    private WorkspaceType type;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(nullable = false)
+    private WorkspaceStatus status;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
 }
