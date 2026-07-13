094ea228e93fdf00cd996ef4899e3650de87e820
AncientSun <70993316+Isle4525@users.noreply.github.com>
2026-07-13 11:28:15 +0500

    feat: create booking model

---
+++ src/main/java/com/example/coworking_booking_system/entity/bookings/Booking.java
@@ -0,0 +1,41 @@
+package com.example.coworking_booking_system.entity.bookings;
+
+import com.example.coworking_booking_system.entity.user.User;
+import com.example.coworking_booking_system.entity.workspace.Workspace;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bookings")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(nullable = false)
+    private BookingStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Workspace workspace;
+}
